cfd6701d083386d955e2b6a47ed057576100baa9
arijit adhikary <arijitadhikary@gmail.com>
2020-02-17 14:07:48 -0500

    Updates

---
+++ src/main/java/com/develop/projectmanagement/service/ProjectService.java
@@ -0,0 +1,13 @@
+package com.develop.projectmanagement.service;
+
+import java.util.List;
+
+import com.develop.projectmanagement.model.Project;
+
+public interface ProjectService {
+
+	public Project addProject(Project project);
+	public Project editProject(Project project);
+	public List<Project> viewProjects();
+	public void deleteProject(Integer projectId);
+}
---
+++ src/main/java/com/develop/projectmanagement/service/ProjectServiceImpl.java
@@ -0,0 +1,43 @@
+package com.develop.projectmanagement.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.develop.projectmanagement.model.Project;
+import com.develop.projectmanagement.repository.ProjectRepository;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	@Override
+	public Project addProject(Project project) {
+		
+		return projectRepository.save(project);
+	}
+
+	@Override
+	public Project editProject(Project project) {
+		
+		return projectRepository.save(project);
+	}
+
+	@Override
+	public List<Project> viewProjects() {
+		List<Project> projectList = new ArrayList<Project>();
+		projectRepository.findAll().forEach(projectList::add);
+		return projectList;
+	}
+
+	@Override
+	public void deleteProject(Integer projectId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
